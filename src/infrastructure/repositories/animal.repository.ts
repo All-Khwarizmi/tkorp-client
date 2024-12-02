db3aebd9e07561363878d202ee71b239ceda296b
Jason Suarez <suarezjason@icloud.com>
2024-12-02 20:04:33 +0100

    📦 feat(repositories): add data access layer for animals and persons

---
+++ src/infrastructure/repositories/animal.repository.ts
@@ -0,0 +1,55 @@
+import { apolloClient } from "../graphql/apollo-client";
+import {
+  ANIMALS_QUERY,
+  ANIMAL_QUERY,
+  STATISTICS_QUERIES,
+} from "../graphql/queries";
+import type {
+  Animal,
+  PaginatedAnimalResponse,
+  AnimalSpeciesCount,
+  OrderByInput,
+} from "../../core/entities/types";
+
+export class AnimalRepository {
+  async getAnimals(
+    page: number,
+    take: number,
+    orderBy?: OrderByInput
+  ): Promise<PaginatedAnimalResponse> {
+    const { data } = await apolloClient.query({
+      query: ANIMALS_QUERY,
+      variables: { page, take, orderBy },
+    });
+    return data.animals;
+  }
+
+  async getAnimal(id: number): Promise<Animal> {
+    const { data } = await apolloClient.query({
+      query: ANIMAL_QUERY,
+      variables: { id },
+    });
+    return data.animal;
+  }
+
+  async getMostCommonSpecies(): Promise<AnimalSpeciesCount[]> {
+    const { data } = await apolloClient.query({
+      query: STATISTICS_QUERIES.MOST_COMMON_SPECIES,
+    });
+    return data.mostCommonSpecies;
+  }
+
+  async getHeaviestAnimal(): Promise<Animal> {
+    const { data } = await apolloClient.query({
+      query: STATISTICS_QUERIES.HEAVIEST_ANIMAL,
+    });
+    return data.heaviestAnimal;
+  }
+
+  async getOldestAnimal(): Promise<Animal> {
+    const { data } = await apolloClient.query({
+      query: STATISTICS_QUERIES.OLDEST_ANIMAL,
+    });
+    return data.oldestAnimal;
+  }
+}
---
+++ src/infrastructure/repositories/person.repository.ts
@@ -0,0 +1,47 @@
+import { apolloClient } from "../graphql/apollo-client";
+import { PERSONS_QUERY, PERSON_QUERY, STATISTICS_QUERIES } from "../graphql/queries";
+import type {
+  Person,
+  PaginatedPersonResponse,
+  OwnershipStats,
+  OwnerWeightStats,
+} from "../../core/entities/types";
+
+export class PersonRepository {
+  async getPersons(page: number, take: number): Promise<PaginatedPersonResponse> {
+    const { data } = await apolloClient.query({
+      query: PERSONS_QUERY,
+      variables: { page, take },
+    });
+    return data.persons;
+  }
+
+  async getPerson(id: number): Promise<Person> {
+    const { data } = await apolloClient.query({
+      query: PERSON_QUERY,
+      variables: { id },
+    });
+    return data.person;
+  }
+
+  async getTopOwner(): Promise<OwnershipStats> {
+    const { data } = await apolloClient.query({
+      query: STATISTICS_QUERIES.TOP_OWNER,
+    });
+    return data.topOwner;
+  }
+
+  async getTopCatOwner(): Promise<OwnershipStats> {
+    const { data } = await apolloClient.query({
+      query: STATISTICS_QUERIES.TOP_CAT_OWNER,
+    });
+    return data.topCatOwner;
+  }
+
+  async getOwnerWithHeaviestPets(): Promise<OwnerWeightStats> {
+    const { data } = await apolloClient.query({
+      query: STATISTICS_QUERIES.OWNER_WITH_HEAVIEST_PETS,
+    });
+    return data.ownerWithHeaviestPets;
+  }
+}
